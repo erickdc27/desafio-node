be3a456be72b993576e4064bfac7e7d197ebd635
Erick Carvalho <101892597+erickdc27@users.noreply.github.com>
2023-05-29 01:03:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ app.get('/', function (req, res) {
 app.listen(8000, function () {
 	console.log('App ouvindo na porta 8000!');
 });
+
+app.post('/post', (req, res) => {
+	res.send(`POST concluido`);
+});
+
+app.delete('/deletar', (req, res) => {
+	res.send(`delete concluido`);
+});
